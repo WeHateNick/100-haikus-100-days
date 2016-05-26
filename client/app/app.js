e3da5d6bea3eca048bdd053dc660105481443faa
Nick Ocampo <nick@wehatenick.com>
2016-05-26 00:05:20 -0700

    installed angular material library and added example html

--- client/app/app.js
+++ client/app/app.js
@@ -2,7 +2,7 @@
 
 angular.module('100Haikus100DaysApp', ['100Haikus100DaysApp.auth', '100Haikus100DaysApp.admin',
     '100Haikus100DaysApp.constants', 'ngCookies', 'ngResource', 'ngSanitize', 'btford.socket-io',
-    'ui.router', 'validation.match'
+    'ui.router', 'validation.match', 'ngMaterial'
   ])
   .config(function($urlRouterProvider, $locationProvider) {
     $urlRouterProvider.otherwise('/');
