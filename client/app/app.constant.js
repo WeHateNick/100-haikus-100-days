0c9579c47890e8626d6ffba974222505e5ae512a
Nick Ocampo <nick@wehatenick.com>
2016-05-25 23:54:36 -0700

    built database

---
+++ client/app/app.constant.js
@@ -0,0 +1,13 @@
+(function(angular, undefined) {
+  angular.module("100Haikus100DaysApp.constants", [])
+
+.constant("appConfig", {
+	"userRoles": [
+		"guest",
+		"user",
+		"admin"
+	]
+})
+
+;
+})(angular);
